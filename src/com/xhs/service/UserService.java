fc1b88b707eacac468fff3bfaee3be76a90ebe4f
熊宝静 <@DESKTOP-IJ5603K>
2018-07-03 21:10:22 +0800

    ssm框架

---
+++ src/com/xhs/service/UserService.java
@@ -0,0 +1,27 @@
+package com.xhs.service;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.xhs.entity.User;
+
+
+public interface UserService {
+	
+	public boolean save(User user);
+	
+	public boolean update(User user);
+	
+	public boolean delete(int id);
+	
+	public User findById(int id);
+	
+	public List<User> findAll();
+	
+	public User checkLogin(String username,String password);
+	
+	public List<User> getAllUserByPage(Integer startRecord,Integer pageSize,String username,String password);
+	
+	public int userCount(String username,String password);
+}
